fix D,R matrix calculation

code/train.py
@@ -115,7 +115,7 @@ class DynamicKGE(nn.Module):
             H_i = H[i][:n+1]
             relation_type = R_i[:n+1, :n+1].reshape(-1).long()    # relation_total*2 + 1: padding index
             w = torch.index_select(weight, 0, relation_type).view(n+1, n+1, config.dim, -1)
-            output_i = F.relu(torch.matmul(H_i.unsqueeze(1), torch.mul(D_i.unsqueeze(-1).unsqueeze(-1), w)).squeeze(-2).sum(1)).sum(0) / n
+            output_i = F.relu(torch.matmul(H_i.unsqueeze(1), torch.mul(D_i.unsqueeze(-1).unsqueeze(-1), w)).squeeze(-2).sum(1))
             output[i] = output_i
 
         return output
@@ -197,14 +197,14 @@ class DynamicKGE(nn.Module):
         pr_adj_relation_vec_list = self.gcn(pr_A, pr_adj_relation_vec_list, target='relation')
         nr_adj_relation_vec_list = self.gcn(nr_A, nr_adj_relation_vec_list, target='relation')
 
-        ph_sg = ph_adj_entity_vec_list
-        pt_sg = pt_adj_entity_vec_list
-        nh_sg = nh_adj_entity_vec_list
-        nt_sg = nt_adj_entity_vec_list
-        # ph_sg = self.calc_subgraph_vec(p_h, ph_adj_entity_vec_list, target='entity')
-        # pt_sg = self.calc_subgraph_vec(p_t, pt_adj_entity_vec_list, target='entity')
-        # nh_sg = self.calc_subgraph_vec(n_h, nh_adj_entity_vec_list, target='entity')
-        # nt_sg = self.calc_subgraph_vec(n_t, nt_adj_entity_vec_list, target='entity')
+        # ph_sg = ph_adj_entity_vec_list
+        # pt_sg = pt_adj_entity_vec_list
+        # nh_sg = nh_adj_entity_vec_list
+        # nt_sg = nt_adj_entity_vec_list
+        ph_sg = self.calc_subgraph_vec(p_h, ph_adj_entity_vec_list, target='entity')
+        pt_sg = self.calc_subgraph_vec(p_t, pt_adj_entity_vec_list, target='entity')
+        nh_sg = self.calc_subgraph_vec(n_h, nh_adj_entity_vec_list, target='entity')
+        nt_sg = self.calc_subgraph_vec(n_t, nt_adj_entity_vec_list, target='entity')
         pr_sg = self.calc_subgraph_vec(p_r, pr_adj_relation_vec_list, target='relation')
         nr_sg = self.calc_subgraph_vec(n_r, nr_adj_relation_vec_list, target='relation')
 

code/util/train_util.py
@@ -150,7 +150,9 @@ def construct_adj_table(train_list, entity_total, relation_total, max_context):
             for index1, neighbour1 in enumerate(neighbours_list):
                 if (entity, neighbour1) in relation_table:
                     R[0][index1+1] = relation_table[(entity, neighbour1)][0]
+                    R[index1+1][0] = relation_table[(entity, neighbour1)][0] + relation_total
                 if (neighbour1, entity) in relation_table:
+                    R[0][index1+1] = relation_table[(neighbour1, entity)][0]
                     R[index1+1][0] = relation_table[(neighbour1, entity)][0] + relation_total
                 if neighbour1 not in entity_adj_table:
                     continue
@@ -160,7 +162,9 @@ def construct_adj_table(train_list, entity_total, relation_total, max_context):
                     if neighbour2 in entity_adj_table[neighbour1]:
                         if (neighbour1, neighbour2) in relation_table:
                             R[index1+1, index2+1] = relation_table[(neighbour1, neighbour2)][0]
+                            R[index2+1, index1+1] = relation_table[(neighbour1, neighbour2)][0] + relation_total
                         if (neighbour2, neighbour1) in relation_table:
+                            R[index1+1, index2+1] = relation_table[(neighbour2, neighbour1)][0]
                             R[index2+1, index1+1] = relation_table[(neighbour2, neighbour1)][0] + relation_total
 
         entity_R[entity] = R
@@ -169,12 +173,11 @@ def construct_adj_table(train_list, entity_total, relation_total, max_context):
     time2 = time.time()
     print("construct R matrix finished. Time elapsed:" + str(time2 - time1))
     print("start to construct D matrix")
-    entity_D = torch.ones(entity_total, max_context_num + 1, max_context_num + 1).cuda()
-    # entity_D = torch.Tensor(entity_total, max_context_num + 1, max_context_num + 1).cuda()
+    entity_D = torch.Tensor(entity_total, max_context_num + 1, max_context_num + 1).cuda()
     for i in range(entity_R.shape[0]):
-        for j in range(entity_R.shape[1]):
-            uni, inv, count = torch.unique(entity_R[i][j], return_inverse=True, return_counts=True)
-            entity_D[i][j] = 1.0/count[inv]
+        for j in range(entity_nn[i]+1):
+            uni, inv, count = torch.unique(entity_R[i][j][:entity_nn[i]+1], return_inverse=True, return_counts=True)
+            entity_D[i][j][:entity_nn[i]+1] = 1.0/count[inv]
     time3 = time.time()
     print("construct D matrix finished. Time elapsed:" + str(time3 - time2))
 