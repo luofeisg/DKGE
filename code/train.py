add activation for GCN and RGCN; bug fix for calculation of A and D matrix of relation

code/train.py
@@ -84,12 +84,12 @@ class DynamicKGE(nn.Module):
         relation_context = self.relation_context.weight
 
         # rgcn
-        entity_context = self.conv1(entity_context, edge_index, edge_type, edge_norm)
+        entity_context = F.relu(self.conv1(entity_context, edge_index, edge_type, edge_norm))
         # entity_context = F.dropout(entity_context, p=0.2, training=self.training)
         # entity_context = self.conv2(entity_context, edge_index, edge_type, edge_norm)
         # gcn
         relation_context = torch.matmul(DAD_rel, relation_context)
-        relation_context = torch.matmul(relation_context, self.relation_gcn_weight)
+        relation_context = F.relu(torch.matmul(relation_context, self.relation_gcn_weight))
 
         # calculate joint embedding
         entity_o = torch.mul(torch.sigmoid(self.gate_entity), entity_emb) + torch.mul(1 - torch.sigmoid(self.gate_entity), entity_context)

code/util/train_util.py
@@ -156,12 +156,25 @@ def generate_graph(triples, relation_total):
     head, tail = np.reshape(entity_idx_inv, (2, -1))
     relabeled_edges = np.stack((head, rel, tail)).transpose()
 
+    # Negative sampling
+    samples, labels = negative_sampling(relabeled_edges, len(uniq_entity), negative_rate=1)
+    samples = torch.from_numpy(samples)
+
+    # further split graph, only half of the edges will be used as graph
+    # structure, while the rest half is used as unseen positive samples
+    split_size = int(triples.shape[0] * 0.5)
+    graph_split_ids = np.random.choice(np.arange(triples.shape[0]), size=split_size, replace=False)
+    head = head[graph_split_ids]
+    tail = tail[graph_split_ids]
+    rel = rel[graph_split_ids]
+
+    split_relabeled_edges = np.stack((head, rel, tail)).transpose()
     # calculate A and D of relation
     relation_entity_table = dict()
     A_rel = torch.eye(relation_total, relation_total).cuda()
     D_rel = np.eye(relation_total, relation_total)
-    for i in range(relabeled_edges.shape[0]):
-        h, r, t = relabeled_edges[i]
+    for i in range(split_relabeled_edges.shape[0]):
+        h, r, t = split_relabeled_edges[i]
         relation_entity_table.setdefault(r, set()).add(h)
         relation_entity_table.setdefault(r, set()).add(t)
     for relation in range(relation_total):
@@ -180,18 +193,6 @@ def generate_graph(triples, relation_total):
 
     DAD_rel = D_rel.mm(A_rel).mm(D_rel)
     # /calculate A and D of relation
-
-    # Negative sampling
-    samples, labels = negative_sampling(relabeled_edges, len(uniq_entity), negative_rate=1)
-    samples = torch.from_numpy(samples)
-
-    # further split graph, only half of the edges will be used as graph
-    # structure, while the rest half is used as unseen positive samples
-    split_size = int(triples.shape[0] * 0.5)
-    graph_split_ids = np.random.choice(np.arange(triples.shape[0]), size=split_size, replace=False)
-    head = head[graph_split_ids]
-    tail = tail[graph_split_ids]
-    rel = rel[graph_split_ids]
 
     head = torch.tensor(head, dtype=torch.long)
     tail = torch.tensor(tail, dtype=torch.long)